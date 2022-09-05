c22ae0db9a148fcef3452f093d3ac0858c53a758
emma673r <90177139+emma673r@users.noreply.github.com>
2022-09-05 22:22:04 +0200

    BACK to done and all -- go to sleep now

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function hexToRGB(hexValue) {
   console.log(`{r: ${r}, g: ${g}, b: ${b}}`);
 
   let rgbObj = { r, g, b };
-  rgbValue.textContent = `rgb(${rgbObj.r}, ${rgbObj.r}, ${rgbObj.r})`;
+  rgbValue.textContent = `rgb(${rgbObj.r}, ${rgbObj.g}, ${rgbObj.b})`;
 
   rgbToHSL(rgbObj);
 }
